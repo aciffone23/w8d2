ccf348ba653b3a588d286b7d0b718f28ce9b3723
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-25 15:25:13 -0400

    myEach

--- phase_1_arrays.js
+++ phase_1_arrays.js
@@ -12,15 +12,61 @@ Array.prototype.myUniq = function () {
 // console.log(test.myUniq());
 
 Array.prototype.twoSum = function () {
-  // let result = [];
+  let result = [];
   for (let i = 0; i < this.length - 1; i++) {
     for (let j = i + 1; j < this.length; j++) {
       if (this[i] + this[j] === 0) {
-        return [i, j];
+        result.push([i, j]);
       }
     }
   }
+  return result;
 };
 
-let test2 = [-1, 4, 1, 0];
-console.log(test2.twoSum());
+// let test2 = [-1, 4, 1, 0, -4];
+// console.log(test2.twoSum());
+
+Array.prototype.transpose = function() {
+  let result = [];
+  let numColLength = this[0].length; 
+  let numRowLength = this.length;
+
+  for (let col = 0; col < numColLength; col++ ){
+    let newRow = [];
+    for (let row = 0; row < numRowLength; row++) {
+      newRow.push(this[row][col]);
+    } 
+    result.push(newRow);
+  }
+  return result;
+}
+
+// let test3 = [[1 , 2 , 3],[4, 5, 6], [7, 8, 9]];
+// console.log(test3.transpose());
+
+Array.prototype.myEach = function(callback){
+  for (let i = 0; i < this.length; i++){
+   callback(this[i]);
+  };
+}
+
+
+
+function double(num) {
+   console.log(num * 2);
+}
+
+function triple(num) {
+  console.log(num * 3);
+}
+
+Array.prototype.myMap = function(callback) {
+  // take in a callback 
+  // use my each to iterate through callback (myEach.callback)
+  // push result into a new array 
+}
+  
+
+let test4 = [1,2,3];
+test4.myEach(double);
+test4.myEach(triple);
